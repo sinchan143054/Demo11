146170931f84464c10a59b11e249e6bcade881d8
sinchan143054 <sinchan.143054@gmial.com>
2025-04-24 07:37:06 +0530

    s

--- Demo11.java
+++ Demo11.java
@@ -1,13 +1,13 @@
- 
-public class Demo {
+public class Demo11 {
     public static void main(String[] args){
         int a=20;
         int b=5;
         int sum=a+b;
         int sub=a-b;
-   
+       
         System.out.println("sum is :"+sum);
         System.out.println("subtraction is :"+sub);
-     
+  
+      
     }
 } 
