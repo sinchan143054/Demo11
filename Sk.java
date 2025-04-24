78970e6021576136a2a40ec7494d5bc95375a30d
sinchan143054 <sinchan.143054@gmial.com>
2025-04-24 07:50:24 +0530

    save

---
+++ Sk.java
@@ -0,0 +1,12 @@
+public class Sk  {
+    public static void main(String[] args){
+        int a=20;
+        int b=5;
+        int sum=a+b;
+        int sub=a-b;
+   
+        System.out.println("sum is :"+sum);
+        System.out.println("subtraction is :"+sub);
+     
+    }
+} 
