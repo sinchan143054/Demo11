31e923bb41efffe83cbaee7592b540c326116ec5
sinchan143054 <sinchan.143054@gmail.com>
2025-04-24 13:29:06 +0530

    Update Demo11.java

--- Demo11.java
+++ Demo11.java
@@ -3,11 +3,11 @@ public class Demo {
     public static void main(String[] args){
         int a=20;
         int b=5;
-        int sum=a+b;
-        int sub=a-b;
+        int mul=a*b;
+        int div=a/b;
    
-        System.out.println("sum is :"+sum);
-        System.out.println("subtraction is :"+sub);
+        System.out.println("multiplication is :"+mul);
+        System.out.println("division is :"+div);
      
     }
 } 
